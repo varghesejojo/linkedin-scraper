0251937055a15219987d436d784f4f41d65f15f8
varghesejojo <varghesejojo6154@gmail.com>
2025-04-20 15:09:44 +0530

    added env and remove the json

--- api/tests.py
+++ api/tests.py
@@ -1,14 +1,18 @@
+import os
 from django.test import TestCase
 from rest_framework.test import APIClient
-
+from dotenv import load_dotenv
+load_dotenv() 
 class LinkedInAPITest(TestCase):
     def setUp(self):
         self.client = APIClient()
+        self.invalid_email = os.getenv("LINKEDIN_EMAIL")
+        self.invalid_password = os.getenv("LINKEDIN_PASSWORD")
 
     def test_login_with_invalid_credentials(self):
         response = self.client.post('/login/', {
-            'username': 'your_email@example.com',
-            'password': 'your_password'
+            'username': self.invalid_email,
+            'password': self.invalid_password
         })
         self.assertEqual(response.status_code, 200)
         self.assertIn("message", response.data)
